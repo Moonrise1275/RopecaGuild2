33fe95d6e003f26e3a0f35779b944b4fedece775
Moonrise1275 <nemo1275@gmail.com>
2014-05-09 17:26:16 +0900

    make ChatRG extends ChatComponentText

--- moonrise/ropecaguild2/api/ChatRG.java
+++ moonrise/ropecaguild2/api/ChatRG.java
@@ -8,18 +8,15 @@ import com.google.common.collect.Lists;
 
 import net.minecraft.event.ClickEvent;
 import net.minecraft.util.ChatComponentStyle;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 
-public class ChatRG implements IChatComponent {
-	
-	private String chat;
-	private List<IChatComponent> siblings = Lists.newArrayList();
-	private ChatStyle style;
+public class ChatRG extends ChatComponentText {
 	
 	public ChatRG(String chat, EnumChatFormatting color, String command) {
-		this.chat = chat;
+		super(chat);
 		if (command != null && !command.isEmpty()) {
 			System.out.println(command + command + command);
 			this.setChatStyle(new ChatStyle().setColor(color).setChatClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command)));
@@ -33,16 +30,6 @@ public class ChatRG implements IChatComponent {
 	
 	public ChatRG(String chat) {
 		this(chat, EnumChatFormatting.WHITE);
-	}
-
-	@Override
-	public String getUnformattedTextForChat() {
-		return chat;
-	}
-
-	@Override
-	public IChatComponent createCopy() {
-		return new ChatRG(this.getFormattedText(), this.getChatStyle().getColor());
 	}
 
 	public enum EnumColor {
@@ -67,51 +54,5 @@ public class ChatRG implements IChatComponent {
 		private EnumColor(EnumChatFormatting e) {
 			this.ecf = e;
 		}
-	}
-
-	@Override
-	public Iterator iterator() {
-		return Iterators.concat(Iterators.forArray(new ChatRG[] {this}), ChatComponentStyle.createDeepCopyIterator(this.siblings));
-	}
-	@Override
-	public IChatComponent setChatStyle(ChatStyle style) {
-		this.style = style;
-		return this;
-	}
-	@Override
-	public ChatStyle getChatStyle() {
-		return this.style;
-	}
-	@Override
-	public IChatComponent appendText(String var1) {
-		this.appendSibling(new ChatRG(var1));
-		return this;
-	}
-	@Override
-	public IChatComponent appendSibling(IChatComponent chat) {
-		this.siblings.add(chat);
-		return this;
-	}
-	@Override
-	public String getUnformattedText() {
-		StringBuilder sb = new StringBuilder(this.chat);
-		for (IChatComponent c : this.siblings) {
-			sb.append(c.getUnformattedText());
-		}
-		return sb.toString();
-	}
-	@Override
-	public String getFormattedText() {
-		StringBuilder sb = new StringBuilder(this.getChatStyle().getFormattingCode());
-		sb.append(this.getUnformattedText());
-		sb.append(EnumChatFormatting.RESET);
-		for (IChatComponent c : this.siblings) {
-			sb.append(c.getFormattedText());
-		}
-		return sb.toString();
-	}
-	@Override
-	public List getSiblings() {
-		return this.siblings;
 	}
 }
